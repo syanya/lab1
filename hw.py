3664b1ccb796993465ab403ef8024eb7c228125f
unknown <oksanochkacov90@gmail.com>
2020-11-06 23:24:49 +0200

    implemented

---
+++ hw.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/api/v1/hello-world-29")
+def hello():
+    return "Hello World 29"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
